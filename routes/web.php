07e3fa8922043f7a745dad1af97f9eaf884df924
Muhammad Afipuddin <apip398@gmail.com>
2024-11-02 09:13:40 +0700

    update middleware

--- routes/web.php
+++ routes/web.php
@@ -8,24 +8,27 @@ use App\Http\Controllers\UserController;
 use Illuminate\Support\Facades\Route;
 
 Route::get("/login", [AuthController::class, 'showLogin'])->name('login');
-Route::post('/login', [AuthController::class,'login']);
-Route::get('/logout', [AuthController::class,'logout'])->name('logout');
+Route::post('/login', [AuthController::class, 'login']);
 
-Route::get('/change-password', [AuthController::class,'showChangePassword'])->name('change-password');
-Route::get('/forgot-password', [AuthController::class,'showForgotPassword'])->name('forgot-password');
+Route::middleware(['isLoginValid'])->group(function () {
+    Route::get('/logout', [AuthController::class, 'logout'])->name('logout');
 
-Route::get("/", [DashboardController::class,"showDashboard"])->name("dashboard");
+    Route::get('/change-password', [AuthController::class, 'showChangePassword'])->name('change-password');
+    Route::get('/forgot-password', [AuthController::class, 'showForgotPassword'])->name('forgot-password');
 
-Route::get("/user", [UserController::class,"showUser"])->name('user');
-Route::get("/user/create", [UserController::class,"create"])->name('user.create');
-Route::post('/user/store', [UserController::class,'store'])->name('user.store');
+    Route::get("/", [DashboardController::class, "showDashboard"])->name("dashboard");
 
-Route::get('/role', [RoleController::class,'showRole'])->name('role');
-Route::get("/role/create", [RoleController::class,"create"])->name('role.create');
-Route::post('/role/store', [RoleController::class,'store'])->name('role.store');
+    Route::get("/user", [UserController::class, "showUser"])->name('user');
+    Route::get("/user/create", [UserController::class, "create"])->name('user.create');
+    Route::post('/user/store', [UserController::class, 'store'])->name('user.store');
 
-Route::get('/upload', [UploadController::class,'showUpload'])->name('upload');
+    Route::get('/role', [RoleController::class, 'showRole'])->name('role');
+    Route::get("/role/create", [RoleController::class, "create"])->name('role.create');
+    Route::post('/role/store', [RoleController::class, 'store'])->name('role.store');
+
+    Route::get('/upload', [UploadController::class, 'showUpload'])->name('upload');
 
 
 
-Route::get("/form", [DashboardController::class,"showForm"])->name('form');
+    Route::get("/form", [DashboardController::class, "showForm"])->name('form');
+});
